0cab1c8b9f2c0d888176eaf100f8df29b7c52f48
spencerwooo <spencerwoo98@foxmail.com>
2020-08-01 17:04:45 +0800

    chore: cleanup and format source

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "vscode-latex-in-readme" is now active!')
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-latex-in-readme" is now active!');
+  let disposable = vscode.commands.registerCommand('vscode-latex-in-readme.render-selection', () => {
+    vscode.window.showInformationMessage('Rendered equation!')
+  })
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-latex-in-readme.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-latex-in-readme!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
